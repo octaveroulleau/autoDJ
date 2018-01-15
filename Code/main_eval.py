d77d0004820d6be5f0a86ea5c55e1357cb5fdcb4
Laure Prétet <laure.pretet@gmail.com>
2018-01-15 09:56:10 +0100

    More structure

---
+++ Code/main_eval.py
@@ -0,0 +1,11 @@
+# Here the file to run to execute the full process in evaluation mode
+
+# Load the data and pre-process them
+
+# Feed the data forward in the CNN
+
+# Feed the data to the VAE
+
+# Evaluate the model :
+# T-SNE
+# MIREVAL
