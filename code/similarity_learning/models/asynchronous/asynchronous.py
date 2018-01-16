ff343e4bc9d1ccccbb0ab6c90bd4e280202a7afc
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-16 19:25:14 +0100

    d

--- code/similarity_learning/models/asynchronous/asynchronous.py
+++ code/similarity_learning/models/asynchronous/asynchronous.py
@@ -17,7 +17,7 @@ def asyncTaskPointer(idx, dataIn, options):
     
 def asynchronous_learning(audioSet, batch_size = 64, nb_epochs = 5):
     asyncTask = AsynchronousTask(asyncTaskPointer, numWorkers = 4, batchSize = 64, shuffle = False)
-    transfi_list, tranform_options  = audioSet.getTransforms()
+    transfi_list, transform_options  = audioSet.getTransforms()
     asyncTask.createTask(audioSet.files, options = {"transformOptions": transform_options})
     for batchIDx, (currentData, currentMeta) in enumerate(asyncTask):
         print('[Batch ' + str(batchIDx) + '] Learning step on ' + str(len(currentData)) + ' examples');
