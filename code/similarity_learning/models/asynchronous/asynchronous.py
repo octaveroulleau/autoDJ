38435cefb22738ffe9b8eedf107bfb63e3d52e1b
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-16 19:09:16 +0100

    d

--- code/similarity_learning/models/asynchronous/asynchronous.py
+++ code/similarity_learning/models/asynchronous/asynchronous.py
@@ -9,10 +9,17 @@ from similarity_learning.models.asynchronous.task import AsynchronousTask
 from data.sets.audio import DatasetAudio
 import pdb
 
+def asyncTaskPointer(idx, dataIn, options):
+    curFile = dataIn
+    print('loading'+ curFile.files[idx])
+    curFile.files = curFile.files[idx]
+    trasnnform_list, transform_options = curFile.getTransforms()
+    curFile.importData(None, {"transformOptions":transformOptions})
+    return curFile
+    
 def asynchronous_learning(audioSet, batch_size = 64, nb_epochs = 5):
-    asyncTask = AsynchronousTask(DatasetAudio.getAsynchronousImport(audioSet), numWorkers = 4, batchSize = batch_size, shuffle = False)
-    options = {}
-    asyncTask.createTask(audioSet.getAsynchronousImport()[0], options)
+    asyncTask = AsynchronousTask(asyncTaskPointer, numWorkers = 4, batchSize = 64, shuffle = False)
+    asyncTask.createTask(audioSet, options = {})
     for batchIDx, (currentData, currentMeta) in enumerate(asyncTask):
         print('[Batch ' + str(batchIDx) + '] Learning step on ' + str(len(currentData)) + ' examples');
         # Perform training on current data 
