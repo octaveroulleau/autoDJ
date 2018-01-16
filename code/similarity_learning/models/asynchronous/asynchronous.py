e2fe366a05e4d37589ebde550438b53581e45e75
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-16 19:24:06 +0100

    d

--- code/similarity_learning/models/asynchronous/asynchronous.py
+++ code/similarity_learning/models/asynchronous/asynchronous.py
@@ -6,23 +6,21 @@ Created on Tue Jan 16 18:06:21 2018
 """
 
 from similarity_learning.models.asynchronous.task import AsynchronousTask
-from data.sets.audio import DatasetAudio
+from data.sets.audio import DatasetAudio, importAudioData
 import pdb
 
 def asyncTaskPointer(idx, dataIn, options):
-    curFile = dataIn
-    print('loading'+ curFile.files[idx])
-    curFile.files = curFile.files[idx]
-    trasnnform_list, transform_options = curFile.getTransforms()
-    curFile.importData(None, {"transformOptions":transformOptions})
-    return curFile
+    print('loading'+ dataIn[idx])
+    
+    data, meta = importAudioData(dataIn, options)
+    return data, meta
     
 def asynchronous_learning(audioSet, batch_size = 64, nb_epochs = 5):
     asyncTask = AsynchronousTask(asyncTaskPointer, numWorkers = 4, batchSize = 64, shuffle = False)
-    asyncTask.createTask(audioSet, options = {})
+    transfi_list, tranform_options  = audioSet.getTransforms()
+    asyncTask.createTask(audioSet.files, options = {"transformOptions": transform_options})
     for batchIDx, (currentData, currentMeta) in enumerate(asyncTask):
         print('[Batch ' + str(batchIDx) + '] Learning step on ' + str(len(currentData)) + ' examples');
-        # Perform training on current data 
         dummy_learn(currentData, currentMeta);
     
     return 0
