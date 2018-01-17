096d5c9f26fb287bc0a545f388ffa069a70c4c58
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-17 14:21:57 +0100

    d

--- code/test_import_TRASH.py
+++ code/test_import_TRASH.py
@@ -4,4 +4,5 @@ from similarity_learning.models.asynchronous.asynchronous import asynchronous_le
 audioSet, audioOptions = data.import_data.import_data()
 #%%
 transform_type, transform_options = audioSet.getTransforms()
+audioSet.files = audioSet.files[1:21]
 asynchronous_learning(audioSet, audioOptions)
