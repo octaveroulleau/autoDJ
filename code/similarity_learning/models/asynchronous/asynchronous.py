b939c9872f952540f2000e6161b8c1f9ec907370
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-17 17:18:28 +0100

    d

--- code/similarity_learning/models/asynchronous/asynchronous.py
+++ code/similarity_learning/models/asynchronous/asynchronous.py
@@ -23,7 +23,7 @@ def asyncTaskPointer(idx, dataIn, options):
     
     data, meta = importAudioData(dataIn, options)
     audioSet.importMetadataTasks();
-    meta = audioSet.metadata['genre'][idx]
+    meta = audioSet.metadata['artist'][idx]
     return data, meta
     
 def asynchronous_learning(audioSet, audioOptions, batch_size = 5, nb_epochs = 5):
