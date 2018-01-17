f884186a4c6da2606e1d073f973842c340af1651
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-17 16:48:33 +0100

    d

--- code/data/import_data.py
+++ code/data/import_data.py
@@ -33,8 +33,8 @@ from data.sets.audio import DatasetAudio
 #from matplotlib import pyplot as plt
 import skimage.transform as skt
 import numpy as np
-#import dielemann.build
-#import keras
+import dielemann.build
+import keras
 #from data_to_phrase import *
 
 
