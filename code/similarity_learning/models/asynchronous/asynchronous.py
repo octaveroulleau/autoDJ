99b33ca782ea4394b9ed8e57169cf094eab9743d
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-17 18:48:00 +0100

    d

--- code/similarity_learning/models/asynchronous/asynchronous.py
+++ code/similarity_learning/models/asynchronous/asynchronous.py
@@ -30,7 +30,7 @@ def asyncTaskPointer(idx, dataIn, options):
 
     return data, meta
     
-def asynchronous_learning(audioSet, audioOptions, nb_frames, model_options, batch_size = 5, nb_epochs = 5):
+def asynchronous_learning(audioSet, audioOptions, nb_frames, model_options, freq_bins = 168, batch_size = 5, nb_epochs = 5):
     '''
     TO DO:
     -Define in call:  number of frames per chunk, type of model, model options
@@ -39,13 +39,13 @@ def asynchronous_learning(audioSet, audioOptions, nb_frames, model_options, batc
     '''
     asyncTask = AsynchronousTask(asyncTaskPointer, numWorkers = 4, batchSize = 5, shuffle = True)
     options = audioOptions
-    options["audioSet"] = audioSet
+    options["audioSet"] = audioSet  
+    alphabet_size = len(set(audioSet.metadata["genre"]))
     
-    pdb.set_trace()
-    data_init, meta = importAudioData(audioSet.files[0], options)
+    model_options["Alphabet size"] = alphabet_size
     
-    
-    #base_model = build.build_conv_layers(nb_frames, )
+    base_model = build.build_conv_layers(nb_frames, freq_bins, model_options)
+    full_layer = build.add_fc_layers(base_model, model_options)
     
 
     for epoch in range(nb_epochs):
@@ -54,15 +54,21 @@ def asynchronous_learning(audioSet, audioOptions, nb_frames, model_options, batc
         for batchIDx, (currentData, currentMeta) in enumerate(asyncTask):
             print('boucle')
             print('[Batch ' + str(batchIDx) + '] Learning step on ' + str(len(currentData)) + ' examples');
-            dummy_learn(currentData, currentMeta);
+            reshape_data(currentData[batchIdx], currentMeta, alphabet_size);
         print('Finished epoch #'+str(epoch))
     
     return 0#model_full, model_base
 
-def dummy_learn(currentData, currentMeta):
+def reshape_data(currentData, currentMeta, alphabet_size):
     #currentData is size (audioSetSize,batchSize,freq,frames)
-    print('Learning on current data - size :');
-    print(len(currentData))
-    # Simulate time
-    print('Learning on ID #' + str(currentMeta[0])) #+ " - 1st elt : " + str(currentData[t][1][1]))
-    print(currentMeta[1])
+    train_test = int((len(currentMeta)-1)*0.85)
+    x_train = np.swapaxes(np.array(currentData[:train_test]),1,2)
+    y_train = np.array(currentMeta[:train_test])
+    x_test = np.swapaxes(np.array(currentData[train_test:]),1,2)
+    y_test = np.array(currentMeta[train_test:])
+    y_train = keras.utils.to_categorical(y_train, alphabet_size)
+    y_test = keras.utils.to_categorical(y_test, alphabet_size)
+    pdb.set_trace()
+    return x_train, y_train, x_test, y_test
+    
+    
