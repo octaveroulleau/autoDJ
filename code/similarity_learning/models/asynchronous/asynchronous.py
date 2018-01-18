3d30aff521a270b9f74bbf8dc349e689d80bdc45
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-18 20:52:49 +0100

    d

--- code/similarity_learning/models/asynchronous/asynchronous.py
+++ code/similarity_learning/models/asynchronous/asynchronous.py
@@ -38,7 +38,7 @@ def asyncTaskPointer(idx, dataIn, options):
     for i in range(len(chunks)):
         chunk = chunks[i].get_cqt(audioSet, options)
         nbBins = chunk.shape[0]
-        chunk = skt.resize(chunk, (nbBins, 100), mode='reflect')
+        chunk = skt.resize(chunk, (nbBins, options["frames number"]), mode='reflect')
         data.append(chunk)
         meta.append(chunks[i].get_meta(audioSet,options['task']))
         
