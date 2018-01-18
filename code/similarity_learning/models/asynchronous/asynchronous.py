a1ed00845a52e527015b1a0903275e4e2c424cfd
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-18 20:51:13 +0100

    d

--- code/similarity_learning/models/asynchronous/asynchronous.py
+++ code/similarity_learning/models/asynchronous/asynchronous.py
@@ -87,7 +87,7 @@ def asynchronous_learning(audioSet, audioOptions, nb_frames, model_options, mode
             print('boucle')
             print('[Batch ' + str(batchIDx) + '] Learning step on ' + str(len(currentData[batchIDx])) + ' examples');
             x_train, y_train = reshape_data(currentData, currentMeta, alphabet_size);
-            history = model_full.fit(x_train, y_train, batch_size = batchSize, epochs = 1, verbose = 1, validation_split = 0.2)
+            history = model_full.fit(x_train, y_train, batch_size = batch_size, epochs = 1, verbose = 1, validation_split = 0.2)
             input('Press enter')
         print('Finished epoch #'+str(epoch))
     
