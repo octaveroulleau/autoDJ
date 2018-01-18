ef4e29e00ca8775a38b143e2b836da2b992101ee
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-18 15:51:02 +0100

    d

--- code/similarity_learning/models/asynchronous/asynchronous.py
+++ code/similarity_learning/models/asynchronous/asynchronous.py
@@ -13,6 +13,7 @@ import time
 import similarity_learning.models.dielemann.build as build
 import numpy as np
 import keras
+import pickle
 
 
 def asyncTaskPointer(idx, dataIn, options):
@@ -27,12 +28,11 @@ def asyncTaskPointer(idx, dataIn, options):
     meta = audioSet.metadata['artist'][idx]
  
     print('loading'+ dataIn[idx]+'\nIndex: '+str(idx)+'\nArtist: '+str(meta))
-    meta = [idx, meta]
     data, meta_trash = importAudioData(dataIn, options)
 
     return data, meta
     
-def asynchronous_learning(audioSet, audioOptions, nb_frames, model_options, freq_bins = 168, batch_size = 5, nb_epochs = 5):
+def asynchronous_learning(audioSet, audioOptions, nb_frames, model_options, model_name, freq_bins = 168, batch_size = 5, nb_epochs = 5):
     '''
     TO DO:
     -Define in call:  number of frames per chunk, type of model, model options
@@ -64,7 +64,7 @@ def asynchronous_learning(audioSet, audioOptions, nb_frames, model_options, freq
 def reshape_data(currentData, currentMeta, alphabet_size):
     #currentData is size (audioSetSize,batchSize,freq,frames)
     pdb.set_trace()
-    train_test = int((len(currentMeta[1])-1)*0.85)
+    train_test = int((len(currentMeta)-1)*0.85)
     x_train = np.swapaxes(np.array(currentData[:train_test]),1,2)
     y_train = np.array(currentMeta[:train_test])
     x_test = np.swapaxes(np.array(currentData[train_test:]),1,2)
@@ -73,5 +73,49 @@ def reshape_data(currentData, currentMeta, alphabet_size):
     y_test = keras.utils.to_categorical(y_test, alphabet_size)
     
     return x_train, y_train, x_test, y_test
+
+def SaveModel(model_full,
+              model_base,
+              name,
+              history,
+              pathmodel='../dielemann/models'):
+    '''
+    Save a model (named with name and the actual date) in the required ./models
+    directory.
+
+    Parameters
+    ----------
+    model: keras.model
+        Model to save
+    name: string
+        Name of the model
+    pathmodel (optionnal): string
+        Path to the models repository
+
+    Returns
+    -------
+    model: keras.model
+        The model we saved.
+    '''
+    print('Save model to ...')
+    date = time.ctime()
+    date = date.replace(' ', '_')
+    date = date.replace(':', '-')
+    print('Save base model as ' + name + '_base_' + date + '.h5' + '...')
+    filepath_base = pathmodel + name + '_base_' + date + '.h5'
+    model_base.save(filepath_base)
+    print('Base model saved in '+ filepath_base)
+
+    print('Save full model as ' + name + '_full_' + date + '.h5' + '...')
+    filepath_full = pathmodel + name + '_full_' + date + '.h5'
+    model_full.save(filepath_full)
+    print('Base model saved in '+ filepath_full)
+    
+    print('Save history as ' + name + '_history_' + date + '...')
+    file = open('../dielemann/models/' + name + '_history_' + date)
+    pickle.dump(history.history, file)
+    print('History saved in' + file)
+    
+    
     
     
