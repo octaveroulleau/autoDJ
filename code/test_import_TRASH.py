54adc32856fd30c51150734184b5a46fd60b1218
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-18 21:11:34 +0100

    d

--- code/test_import_TRASH.py
+++ code/test_import_TRASH.py
@@ -3,13 +3,19 @@ import tensorflow
 import keras
 import data
 from similarity_learning.models.asynchronous.asynchronous import asynchronous_learning
-
+from keras.backend.tensorflow_backend import set_session
 #%%
 
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+#config.gpu_options.per_process_gpu_memory_fraction = 0.3
+config.gpu_options.visible_device_list = "2"
+set_session(tf.Session(config=config))
+#%%
 audioSet, audioOptions = data.import_data.import_data()
 #%%
 transform_type, transform_options = audioSet.getTransforms()
-audioSet.files = audioSet.files[0:300]
+audioSet.files = audioSet.files[0:10]
 nb_frames = 100
 mod_options = {
         'activation': 'relu',
