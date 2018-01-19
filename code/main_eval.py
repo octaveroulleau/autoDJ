3c7b1525e05697a0c7b92ae8dc98777c8673c86f
pagrumiaux <pierreamaury.grumiaux@gmail.com>
2018-01-19 18:03:08 +0100

    wip

--- code/main_eval.py
+++ code/main_eval.py
@@ -14,28 +14,11 @@
 # BEATS EVALUATION
 #######################
 
-# load reference data
-beats_ref = load_beats_reference()
+beat_evaluation()
 
-# compute beats with several algorithms
-beats_madmom = compute_beats_madmom()
-beats_librosa = compute_beats_librosa()
-beats_ellis = compute_beats_ellis()
-
-# calculate score
-beats_score_madmom = mir_eval_beats(beats_ref, beats_madmom)
-beats_score_librosa = mir_eval_beats(beats_ref, beats_librosa)
-beats_score_ellis = mir_eval_beats(beats_ref, beats_ellis)
-
-#########################
+#######################
 # DOWNBEATS EVALUATION
-#########################
+#######################
 
-# load reference data
-downbeats_ref = load_downbeats_reference()
+downbeat_evaluation()
 
-# compute downbeats with several algorithms
-downbeats_madmom = compute_downbeats_madmom()
-
-# calculate score
-downbeats_score_madmom = mir_eval_downbeats(downbeats_ref, downbeats_madmom)
