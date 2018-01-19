c6ef9fc0383b4f157eac0577e8a0401009894738
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-19 02:00:31 +0100

    d

--- code/similarity_learning/models/asynchronous/asynchronous.py
+++ code/similarity_learning/models/asynchronous/asynchronous.py
@@ -86,7 +86,7 @@ def asynchronous_learning(audioSet, audioOptions, nb_frames, model_options, mode
         asyncTask.createTask(audioSet.files, options)
         print('Epoch #' + str(epoch));
         for batchIDx, (currentData, currentMeta) in enumerate(asyncTask):
-            print('boucle')
+            pdb.set_trace()
             print('[Batch ' + str(batchIDx) + '] Learning step on ' + str(len(currentData[batchIDx])) + ' examples');
             x_train, y_train = reshape_data(currentData, currentMeta, alphabet_size);
             history = model_full.fit(x_train, y_train, batch_size = batch_size, epochs = 1, verbose = 1, validation_split = 0.2)
