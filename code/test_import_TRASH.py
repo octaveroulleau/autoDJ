1f43f8335a1df0f8e3474b2700212447d744475d
Octave Roulleau <octave.roulleau@gmail.com>
2018-01-19 18:43:32 +0100

    CNN process

--- code/test_import_TRASH.py
+++ code/test_import_TRASH.py
@@ -9,7 +9,7 @@ from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 #config.gpu_options.per_process_gpu_memory_fraction = 0.3
-config.gpu_options.visible_device_list = "2"
+config.gpu_options.visible_device_list = "1"
 set_session(tf.Session(config=config))
 
 #%%
